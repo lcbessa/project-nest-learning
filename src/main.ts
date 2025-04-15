d36a14b3b92257182ec04a563750720a65e49c5a
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2025-04-15 17:34:33 -0300

    refactor: adicionar tipagem NestExpressApplication para explorar recursos específicos do Express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
   if (module.hot) {
