14447811ea8b7e5e991503810e1262f70097f5a8
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2025-04-23 17:25:54 -0300

    feat: criar resource produtos

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,13 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Nest application failed to start', err);
+});
